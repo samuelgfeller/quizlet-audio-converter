d22bca604fee065b0c9e02bc2504fe3b41b9551c
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-02-25 09:43:52 +0100

    separated in blocks

--- backend/Converter.php
+++ backend/Converter.php
@@ -9,6 +9,19 @@ class Converter
     {
         $this->config = $config;
     }
+
+    /**
+     * Create directory if it doesn't exist already
+     *
+     * @param string $dir
+     */
+    private function createDirectory(string $dir)
+    {
+        if (!file_exists($dir) && !mkdir($concurrentDirectory = $dir,
+                0777, true) && !is_dir($concurrentDirectory)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
+        }
+    }
     
     /**
      * Generates silence file
@@ -18,6 +31,8 @@ class Converter
      */
     public function generateSilence(string $silenceName, int $duration)
     {
+        $this->createDirectory($this->config['silence_dir']);
+
         $cmd = 'ffmpeg -f lavfi -y -i anullsrc=channel_layout=5.1:sample_rate=32000 -t ' . $duration . ' ' . $this->config['silence_dir'] . '/' . $silenceName;
         shell_exec($cmd);
     }
@@ -42,15 +57,24 @@ class Converter
         // return all cards with their mapping as array
         return json_decode($removedAfterAndBefore, true)['termIdToTermsMap'];
     }
-    
+
     /**
      * Prepare file which contains list of audio files
-     * in the right order with silences
-     * @param $allCards
+     * in the right order with silences.
+     *
+     * The rule is that each 20 words are played in a loop (21 times).
+     * For that they are grouped and saved in the 2d array allBlocks
+     * and written to the corresponding block files and the filenames
+     * are returned.
+     *
+     * @param array $allCards
+     * @return array
      */
-    public function prepareAudioListFile($allCards)
+    public function prepareAudioBlockFiles(array $allCards)
     {
-        $audioPaths = [];
+        $cardsAmount = count($allCards);
+        $allBlocks = [];
+        $iteratingBlockValues = [];
         foreach ($allCards as $card) {
             // The is either without the base (when its quizlet.com domain) or full url when the audio is on amazon server
             $wordAudioUrl = strpos(
@@ -63,21 +87,61 @@ class Converter
             ) !== false ? $card['_definitionAudioUrl'] : $this->config['quizlet_domain'] . $card['_definitionAudioUrl'];
             
             // Relative paths are mandatory for the command
-            $audioPaths[] = "file '../silences/long-silence.mp3'";
-            $audioPaths[] = "file '$wordAudioUrl'";
-            $audioPaths[] = "file '../silences/long-silence.mp3'";
-            $audioPaths[] = "file '$definitionAudioUrl'";
+            $iteratingBlockValues[] = "file '../silences/long-silence.mp3'";
+            $iteratingBlockValues[] = "file '$wordAudioUrl'";
+            $iteratingBlockValues[] = "file '../silences/short-silence.mp3'";
+            $iteratingBlockValues[] = "file '$definitionAudioUrl'";
             //    echo '<a href="'.$wordAudioUrl.'">'.$card['word'].'</a> | <a href="'.$definitionAudioUrl.'">'.$card['definition'].'</a><br>';
+
+            // 4 lines are added each time so to have 20 words the number has to be multiplied by 4
+            // Check array contains
+            if ($cardsAmount >= 20 && count($iteratingBlockValues) === 80) {
+                // Save the cards from the first block to the general array
+                $allBlocks[] = $iteratingBlockValues;
+
+                // Reset cards for iterating block
+                $iteratingBlockValues = [];
+                // Remove 20 to the card amount
+                $cardsAmount -= 20;
+            }
+            // If the amount of cards is less than 20 a block has to be filled with the last cards
+            elseif ($cardsAmount < 20 && count($iteratingBlockValues) === $cardsAmount * 4) {
+                // Save the cards from the first block to the general array
+                $allBlocks[] = $iteratingBlockValues;
+                // Reset cards for iterating block
+                $iteratingBlockValues = [];
+                // Remove 20 to the card amount
+                $cardsAmount -= $cardsAmount;
+            }
+
         }
-        
-        // Populate the files.txt with all the paths to the audio files
-        file_put_contents($this->config['output_dir'] . '/files.txt', implode(PHP_EOL, $audioPaths));
+
+        // Create output folder if it doesn't exist
+        $this->createDirectory($this->config['output_dir']);
+
+        $allFileBlocks = [];
+        $i = 1;
+        foreach ($allBlocks as $block){
+            $fileNameWithoutExtension = 'block-'.$i;
+            // Populate the files.txt with all the paths to the audio files
+            file_put_contents($this->config['output_dir'] . '/'.$fileNameWithoutExtension.'.txt', implode(PHP_EOL, $block));
+            $allFileBlocks[] = $fileNameWithoutExtension;
+            $i++;
+        }
+
+        return $allFileBlocks;
     }
-    
-    public function concatAndOutputAudio()
+
+    /**
+     * Put audio files together
+     *
+     * @param string $inputFileListName MUST be in the output folder
+     * @param string $outputName WILL be in the output folder
+     */
+    public function concatAndOutputAudio(string $inputFileListName, string $outputName)
     {
         $cmd = 'ffmpeg -f concat -safe 0 -protocol_whitelist file,http,https,tcp,tls -y -i ' .
-            $this->config['output_dir'] . '/files.txt' . ' -c copy ' . $this->config['output_dir'] . '/output/all.mp3';
+            $this->config['output_dir'] . '/' .$inputFileListName . ' -c copy ' . $this->config['output_dir'] . '/'.$outputName;
         shell_exec($cmd);
     }
 }
--- index.php
+++ index.php
@@ -7,8 +7,9 @@ $config = include __DIR__ . '/config/public_config.php';
 
 $converter = new Converter($config);
 
-$quizletUrl = 'https://quizlet.com/es/487468389/test-portugisisch-flash-cards/';
-//$quizletUrl = 'https://quizlet.com/484664798/french-flash-cards/';
+//$quizletUrl = 'https://quizlet.com/es/487468389/test-portugisisch-flash-cards/';
+$quizletUrl = 'https://quizlet.com/484664798/french-flash-cards/';
+
 $betweenWordSilenceDuration = 1;
 $nextSetSilenceDuration = 2;
 
@@ -18,9 +19,20 @@ $allCards = $converter->retrieveCardInfos($quizletUrl);
 $converter->generateSilence('short-silence.mp3',$betweenWordSilenceDuration);
 $converter->generateSilence('long-silence.mp3',$nextSetSilenceDuration);
 
-$converter->prepareAudioListFile($allCards);
+$blockFiles = $converter->prepareAudioBlockFiles($allCards);
 
-$converter->concatAndOutputAudio();
+$linesForFinalFile = [];
+foreach ($blockFiles as $file){
+    $converter->concatAndOutputAudio($file.'.txt',$file.'.mp3');
+    // The files are in the output so the relative path to this file is just the file name
+    $linesForFinalFile[] = "file '$file.mp3'";
+}
+
+
+file_put_contents($config['output_dir'] . '/final-file.txt', implode(PHP_EOL, $linesForFinalFile));
+
+$converter->concatAndOutputAudio('final-file.txt','final.mp3');
+
 
 
 
