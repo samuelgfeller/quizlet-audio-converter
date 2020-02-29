7c27e269232bee68867268d954cb9b32c932261c
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-02-29 20:47:15 +0100

    made downloaded button responsive

--- frontend/download_list.php
+++ frontend/download_list.php
@@ -4,13 +4,13 @@ $pathControlFile = '../output/control.mp3';
 $pathFinalFile = '../output/final.mp3';
 
 if (file_exists($pathControlFile)){
-    echo '<a href="'.$pathControlFile.'"><p class="infoP">Alle Worte ohne Widerholung (Kontrolle)</p></a>';
+    echo '<a href="'.$pathControlFile.'" target="_blank"><p class="infoP">Alle Worte ohne Widerholung (Kontrolle)</p></a>';
 }else{
     echo '<p class="infoP">Kontroll-Datei nicht gefunden</p>';
 }
 
 if (file_exists($pathFinalFile)){
-    echo '<a href="'.$pathFinalFile.'"><p class="infoP">Datei mit Wiederholungen</p></a>';
+    echo '<a href="'.$pathFinalFile.'" target="_blank"><p class="infoP">Datei mit Wiederholungen</p></a>';
 }else{
     echo '<p class="infoP">Datei mit Wiederholungen nicht gefunden</p>';
 
