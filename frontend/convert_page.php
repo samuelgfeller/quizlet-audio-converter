214c4b879e4fe9d54490a3eb53b293e56a638f26
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-03-24 21:21:04 +0100

    Added favicon

--- frontend/convert_page.php
+++ frontend/convert_page.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" type="image/png" href="../frontend/img/favicon.ico"/>
     <title>Quizlet Audio</title>
 </head>
 <body>
 <?php
+
 require_once __DIR__ . '/../logic/convert.php';
 require_once __DIR__ . '/download_list.php';
 ?>
 </body>
 </html>
-
-
